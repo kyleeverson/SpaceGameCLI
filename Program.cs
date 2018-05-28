2b379f9e70c9922ad37c68881d59c396594f8208
Kyle Everson <kyle.everson@csra.com>
2018-05-27 23:45:30 -0700

    Initial Checkin

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace spacegame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
